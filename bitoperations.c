807fb1bf22bf09cccf0972bd2a203655d88c0891
CathalDonohoe <cdonohoe1374@gmail.com>
2021-02-01 16:06:06 +0000

    Halfway through lab

--- bitoperations.c
+++ bitoperations.c
@@ -2,8 +2,22 @@
 
 int main(int argc, char *argv[]){
     
-    printf("%s\n", "Hello World");
+    int i = 0xF1;
+
+    printf("Dec: %d\n", i);
+    printf("Hex: %x\n", i);
+    printf("Size of int: %d\n", sizeof(i));
+    printf("Size of int: %d\n", sizeof(char));
     
-    
+
+    char c = 41;
+    printf("c in char is: %c\n", c);
+    printf("c in int is: %d\n", c);
+
+    int j = 1000000000;
+    printf("j in char is: %c\n", j);
+    printf("j in int is: %d\n", j);
+
+
     return 0;
 }
