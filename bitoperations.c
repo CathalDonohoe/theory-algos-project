a488814dabc3cf099f1fff70a217b79f35ca1f6b
CathalDonohoe <cdonohoe1374@gmail.com>
2021-02-08 15:34:02 +0000

    part 1 done

--- bitoperations.c
+++ bitoperations.c
@@ -1,32 +1,42 @@
 #include <stdio.h>
 
-void bin_print(int i){
-    int j = sizeof(int) * 8;
+void bin_print(unsigned int i){
+    // Number of bits in an Integer
+    int j = sizeof(unsigned int) * 8;
 
-
+    //Temporary Variable
     int k;
 
+    // Loop over the number of bits in i.
     for(j--;j>=0;j--){
+        //
         k = ((1<< j) & i)? 1 : 0;
+        //print k
         printf("%d", k);
     }
 }
 
-
+// Standard main structure
 int main(int argc, char *argv[]){
-    
-    int i = 0xf1;
+    // Set i to a literal value
+    unsigned int i = 4294967295;//1; // 0xf1; // 241 // 8589934591
 
+    // whaty we're printing
     printf("Original:   ");
-
+    // print i in binary
     bin_print(i);
-    printf("\n");
+    // end line
+    printf("\t%x\t%u\n", i, i);
 
     for(int j = 0; j < 40; j++){
-        printf("%8d << %2d: ", i, j);
+        //what the operation is
+        printf("%3u << %2d: ", i, j);
+        // i shifted left j times
         bin_print(i << j);
+        //end line
         printf("\n");
     }
 
+    //evertring is okay
     return 0;
 }
