d93549b26d7cda1b4c25da06965927dc19bea777
CathalDonohoe <cdonohoe1374@gmail.com>
2021-02-08 16:14:43 +0000

    Bit masks

--- bitoperations.c
+++ bitoperations.c
@@ -8,8 +8,8 @@ void bin_print(unsigned int i){
     int k;
 
     // Loop over the number of bits in i.
-    for(j--;j>=0;j--){
-        //
+    for(j --; j >= 0; j--){
+        // pick out the j^th bit of i, left to right
         k = ((1<< j) & i)? 1 : 0;
         //print k
         printf("%d", k);
@@ -19,22 +19,26 @@ void bin_print(unsigned int i){
 // Standard main structure
 int main(int argc, char *argv[]){
     // Set i to a literal value
-    unsigned int i = 4294967295;//1; // 0xf1; // 241 // 8589934591
+    unsigned int i = 0x0f0f0f0f;//1; // 0xf1; // 241 // 8589934591
 
     // whaty we're printing
-    printf("Original:   ");
+    printf("Original:\t");
     // print i in binary
     bin_print(i);
     // end line
-    printf("\t%x\t%u\n", i, i);
+    printf("\t%x\t%u\n\n", i, i);
 
-    for(int j = 0; j < 40; j++){
-        //what the operation is
-        printf("%3u << %2d: ", i, j);
+    //32
+    int j = sizeof(unsigned int) * 8;
+
+    for(j--; j >= 0; j--){
         // i shifted left j times
-        bin_print(i << j);
-        //end line
-        printf("\n");
+        bin_print(i << j); printf("\n");
+        //i
+        bin_print(i); printf("\n");
+        printf("-------------------------- &\n");
+        // (1 shifted left j times) bitwise logical and i.
+        bin_print((1 << j) & i); printf("\n\n");
     }
 
     //evertring is okay
