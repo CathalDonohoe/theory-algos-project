5827396d7c193c84a2e01113f4a8ee5eee4d6074
CathalDonohoe <cdonohoe1374@gmail.com>
2021-02-08 16:37:18 +0000

    Fixed error, an i was a 1

--- bitoperations.c
+++ bitoperations.c
@@ -8,16 +8,16 @@ void bin_print(unsigned int i){
     int k;
 
     // Loop over the number of bits in i.
-    for(j --; j >= 0; j--){
+    for(j--; j >= 0; j--){
         // pick out the j^th bit of i, left to right
-        k = ((1<< j) & i)? 1 : 0;
+        k = ((1<< j) & i) ? 1 : 0;
         //print k
         printf("%d", k);
     }
 }
 
 // Standard main structure
-int main(int argc, char *argv[]){
+int main(int argc, char const *argv[]){
     // Set i to a literal value
     unsigned int i = 0x0f0f0f0f;//1; // 0xf1; // 241 // 8589934591
 
@@ -33,7 +33,7 @@ int main(int argc, char *argv[]){
 
     for(j--; j >= 0; j--){
         // i shifted left j times
-        bin_print(i << j); printf("\n");
+        bin_print(1 << j); printf("\n");
         //i
         bin_print(i); printf("\n");
         printf("-------------------------- &\n");
