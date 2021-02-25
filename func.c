edf88a4f407501d1c73c68d914c21f5c43e69d97
CathalDonohoe <cdonohoe1374@gmail.com>
2021-02-25 18:03:46 +0000

    changed to display hex values

--- func.c
+++ func.c
@@ -12,11 +12,11 @@ uint32_t Ch(uint32_t x, uint32_t y, uint32_t z){
 int main(int argc, char *argv[]){
 
     uint32_t x = 0xf0f0f0f0;
-    uint32_t y = 0xf0f0f0f0;
+    uint32_t y = 0x0f0f0f0f;
     uint32_t z = 0xf0f0f0f0;
 
     uint32_t ans = Ch(x,y,z);
 
-    printf("Ch(%" PRIu32 ",%" PRIu32 ",%" PRIu32 ")= %" PRIu32 "\n", x,y,z,ans);
+    printf("Ch(%08" PRIX32 ",%08" PRIX32 ",%08" PRIX32 ")= %08" PRIX32 "\n", x,y,z,ans);
     return 0;
 }
