be5b6295ece8ae802686afbdc00b22369c9035bc
CathalDonohoe <cdonohoe1374@gmail.com>
2021-02-25 18:12:32 +0000

    changed output and defined keywords. Lab Done

--- func.c
+++ func.c
@@ -2,8 +2,9 @@
 #include <inttypes.h>
 
 #define WORD uint32_t
+#define PF PRIX32
 
-uint32_t Ch(uint32_t x, uint32_t y, uint32_t z){
+WORD Ch(WORD x, WORD y, WORD z){
     return (x&y)^(~x&z);
 }
 
@@ -11,12 +12,12 @@ uint32_t Ch(uint32_t x, uint32_t y, uint32_t z){
 
 int main(int argc, char *argv[]){
 
-    uint32_t x = 0xf0f0f0f0;
-    uint32_t y = 0x0f0f0f0f;
-    uint32_t z = 0xf0f0f0f0;
+    WORD x = 0x0F0F0F0F;
+    WORD y = 0x0A0A0A0A;
+    WORD z = 0xB0B0B0B0;
 
-    uint32_t ans = Ch(x,y,z);
+    WORD ans = Ch(x,y,z);
 
-    printf("Ch(%08" PRIX32 ",%08" PRIX32 ",%08" PRIX32 ")= %08" PRIX32 "\n", x,y,z,ans);
+    printf("Ch(%08" PF ",%08" PF ",%08" PF ")= %08" PF "\n", x,y,z,ans);
     return 0;
 }
