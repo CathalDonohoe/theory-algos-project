9935b4dcb1f8b06cd2209220391681408efd5449
CathalDonohoe <cdonohoe1374@gmail.com>
2021-03-01 14:43:58 +0000

    Made MAJ function

--- func.c
+++ func.c
@@ -4,6 +4,7 @@
 #define WORD uint32_t
 #define PF PRIX32
 #define CH(x,y,z) (x&y)^(~x&z)
+#define MAJ(x,y,z) (x&y)^(x&z)^(y&z)
 
 
 
@@ -15,8 +16,11 @@ int main(int argc, char *argv[]){
     WORD y = 0x0A0A0A0A;
     WORD z = 0xB0B0B0B0;
 
-    WORD ans = CH(x,y,z);
+    WORD ans; 
+    ans = CH(x,y,z);
 
     printf("CH(%08" PF ",%08" PF ",%08" PF ")= %08" PF "\n", x,y,z,ans);
+    ans = MAJ(x,y,z);
+    printf("MAJ(%08" PF ",%08" PF ",%08" PF ")= %08" PF "\n", x,y,z,ans);
     return 0;
 }
